a14dadea1b05db89e8e30e4b77872310c947d4ce
scole02 <samcole107@gmail.com>
2024-02-08 22:18:01 -0800

    preliminary azure deployment stuff

--- alzi/alzi/wsgi.py
+++ alzi/alzi/wsgi.py
@@ -11,6 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'alzi.settings')
+settings_module = 'alzi.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'alzi.settings'
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_wsgi_application()
