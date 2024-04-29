f4c72dfbd9b3a48c4a49765dfb6157e1552daf3c
scole02 <50273323+scole02@users.noreply.github.com>
2024-04-29 16:03:02 -0700

    Add CORS whitelisting

--- alzi/alzi/deployment.py
+++ alzi/alzi/deployment.py
@@ -3,6 +3,7 @@ from .settings import *
 from .settings import BASE_DIR
 
 ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
+CORS_ORIGIN_WHITELIST = ['https://witty-pebble-0b4c4811e.4.azurestaticapps.net/', 'http://' + os.environ['WEBSITE_HOSTNAME'] ]
 CSRF_TRUSTED_ORIGINS = ['http://' + os.environ['WEBSITE_HOSTNAME']]
 DEBUG = False
 
