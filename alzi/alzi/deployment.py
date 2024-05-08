964957e8a7f013b4140e971e5e8b3cbc2039fc84
scole02 <samcole107@gmail.com>
2024-05-08 09:43:59 -0700

    update CSRF and CORS whitelists

--- alzi/alzi/deployment.py
+++ alzi/alzi/deployment.py
@@ -3,8 +3,8 @@ from .settings import *
 from .settings import BASE_DIR
 
 ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
-CORS_ORIGIN_WHITELIST = ['https://witty-pebble-0b4c4811e.4.azurestaticapps.net/', 'http://' + os.environ['WEBSITE_HOSTNAME'] ]
-CSRF_TRUSTED_ORIGINS = ['http://' + os.environ['WEBSITE_HOSTNAME']]
+CORS_ORIGIN_WHITELIST = ['https://witty-pebble-0b4c4811e.4.azurestaticapps.net/', 'https://' + os.environ['WEBSITE_HOSTNAME'] ]
+CSRF_TRUSTED_ORIGINS = ['https://' + os.environ['WEBSITE_HOSTNAME']]
 DEBUG = False
 
 OPENAI_SECRET_KEY = os.environ['OPENAI_SECRET_KEY']
